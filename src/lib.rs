8e1a8fd0ba277d0afeebacf6a4929aa13ad7292b
Konstantin Senkevich <konstsen@gmail.com>
2021-01-30 14:44:53 +0300

    Improved the converter.

--- src/lib.rs
+++ src/lib.rs
@@ -3,17 +3,28 @@ pub mod sniffer;
 pub mod trader;
 pub mod merchant;
 
+use agnostic::coin::Coin;
+
 #[derive(Clone, Copy, Debug)]
 pub struct CoinConverter {
 }
 
 impl agnostic::coin::CoinConverter for CoinConverter {
+    type Coin = chatex_sdk_rust::coin::Coin;
+
     fn to_string(&self, coin: agnostic::coin::Coin) -> String {
-        use agnostic::coin::Coin;
         match coin {
             Coin::TON => chatex_sdk_rust::coin::Coin::TON.to_string(),
             Coin::USDT => chatex_sdk_rust::coin::Coin::USDT.to_string(),
             Coin::BTC => chatex_sdk_rust::coin::Coin::BTC.to_string(),
+        }
+    }
+
+    fn to_coin(&self, coin: agnostic::coin::Coin) -> Self::Coin {
+        match coin {
+            Coin::TON => chatex_sdk_rust::coin::Coin::TON,
+            Coin::USDT => chatex_sdk_rust::coin::Coin::USDT,
+            Coin::BTC => chatex_sdk_rust::coin::Coin::BTC,
         }
     }
 }
